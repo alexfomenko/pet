1a40c3f90de348399d03ca213616399de5ec08b7
Oleksandra Fomenko <fomenkoalexandra92@gmail.com>
2025-12-26 17:58:40 +0200

    Added toasts to sign in

--- views/logInView.js
+++ views/logInView.js
@@ -2,6 +2,7 @@ import {userLogIn} from '../api/authApi.js';
 
 let button = document.getElementById('sign-in-button');
 let error = document.getElementById("loginError");
+// let toast = document.getElementById("toast");
 
 button.addEventListener('click', async(e) => {
     e.preventDefault();
@@ -10,10 +11,34 @@ button.addEventListener('click', async(e) => {
 
     let sendSignInRequest = await userLogIn(email, password);
 
+    //showing error
+    // if(!sendSignInRequest.success) {
+    //     error.textContent = sendSignInRequest.message;
+    // }
+
+    //showing toast error
     if(!sendSignInRequest.success) {
-        error.textContent = sendSignInRequest.message;
+        // toast.textContent = sendSignInRequest.message;
+        // toast.classList.add("show");
+        //
+        // clearTimeout(toast.timerId);
+        // toast.timerId = setTimeout(()=> toast.classList.remove("show"), 2500)
+        showToastError(sendSignInRequest.message)
+        return;
     }
+
+    showToastError(sendSignInRequest.message)
 
     //set token
     localStorage.setItem("token", sendSignInRequest.token)
 })
+
+function showToastError(text, ms =2500) {
+    let toast = document.getElementById("toast");
+    // toast.textContent = sendSignInRequest.message;
+    toast.textContent = text;
+    toast.classList.add("show");
+
+    clearTimeout(toast.timerId);
+    toast.timerId = setTimeout(()=> toast.classList.remove("show"), ms)
+}
