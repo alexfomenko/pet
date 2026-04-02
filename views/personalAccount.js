2a25c0fc893f6e6493911909e7d9b00e7aa67526
Oleksandra Fomenko <fomenkoalexandra92@gmail.com>
2026-04-02 12:13:56 +0300

    Added js to tabs

---
+++ views/personalAccount.js
@@ -0,0 +1,9 @@
+let tabs = document.querySelectorAll(".tab");
+tabs.forEach((tab) => {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        })
+        tab.classList.add("active");
+    })
+})
