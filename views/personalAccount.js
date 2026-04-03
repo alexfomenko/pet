3964e351fd1c7ad92b005c457c6ecd6211fa9197
Oleksandra Fomenko <fomenkoalexandra92@gmail.com>
2026-04-03 11:23:44 +0300

    Added js to personal account header

--- views/personalAccount.js
+++ views/personalAccount.js
@@ -7,3 +7,54 @@ tabs.forEach((tab) => {
         tab.classList.add("active");
     })
 })
+
+let editBtn = document.querySelector('.edit-btn');
+let fillInBtn = document.querySelector('.fill-btn');
+let nameEl = document.querySelector('.person-name');
+let titleEl = document.querySelector('.person-title');
+let emailEl = document.querySelector('.person-email');
+let saveBtn;
+let header = document.querySelector('.personalAccount-header');
+// editBtn.addEventListener('click', (e) => {
+//     // let nameValue = nameEl.textContent;
+//     // let titleValue = titleEl.textContent;
+//     // let emailValue = emailEl.textContent;
+//     //
+//     nameEl.innerHTML = `<input type="text" value="${nameEl.textContent}">`;
+//     titleEl.innerHTML = `<input type="text" value="${titleEl.textContent}">`;
+//     emailEl.innerHTML = `<input type="text" value="${emailEl.textContent}">`;
+//
+//     editBtn.style.display = "none";
+//
+//     saveBtn = document.createElement('button');
+//     saveBtn.classList.add('btn');
+//     saveBtn.textContent = "Save";
+//     header.appendChild(saveBtn);
+// })
+
+header.addEventListener('click', (e) => {
+    if(e.target.classList.contains('edit-btn')) {
+        nameEl.innerHTML = `<input type="text" value="${nameEl.textContent}">`;
+        titleEl.innerHTML = `<input type="text" value="${titleEl.textContent}">`;
+        emailEl.innerHTML = `<input type="text" value="${emailEl.textContent}">`;
+
+        editBtn.style.display = "none";
+
+        saveBtn = document.createElement('button');
+        saveBtn.classList.add('save-btn','btn');
+        saveBtn.textContent = "Save";
+        header.appendChild(saveBtn);
+    }
+    else if(e.target.classList.contains('save-btn')) {
+        // nameEl.textContent = nameEl.querySelector('input').value;
+        // titleEl.textContent = titleEl.querySelector('input').value;
+        // emailEl.textContent = emailEl.querySelector('input').value;
+
+        nameEl.textContent = nameEl.querySelector('input').value;
+        titleEl.textContent = titleEl.querySelector('input').value;
+        emailEl.textContent = emailEl.querySelector('input').value;
+
+        editBtn.style.display = 'inline-block';
+        saveBtn.style.display = 'none';
+    }
+})
