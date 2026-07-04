2f8ddadedba9eecda4b0d05bcb8efed48b176442
Oleksandra Fomenko <fomenkoalexandra92@gmail.com>
2026-07-04 14:35:55 +0300

    refactored companyAboutView

--- views/companyAboutView.js
+++ views/companyAboutView.js
@@ -12,8 +12,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     let params = new URLSearchParams(location.search);
     let company = params.get('company');
 
+    if (!company) {
+        document.querySelector(".company-name").textContent = "Company not selected";
+        document.querySelector(".company-logo").textContent = " ";
+        document.querySelector(".company-about .review-text").textContent = "Please select a company from reviews.";
+        renderReviewsV2([]);
+        return;
+    }
+
     let companyData = await getCompanyAboutData(company);
-    renderCompanyAbout(companyData);
+    if(companyData.status === 404) {
+        document.querySelector(".company-name").textContent = company;
+        document.querySelector(".company-logo").textContent = company;
+        document.querySelector(".company-about .review-text").textContent = "Company not registered";
+    }
+    else{
+        renderCompanyAbout(companyData);
+    }
 
     let companyReviews = await getCompanyReviews(company, appState.currentPage, appState.currentPageLimit, appState.filter, appState.sort);
     renderReviewsV2(companyReviews.items);
